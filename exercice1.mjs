b85ad13a764b4c37190f99e998c04007acfb5db2
Yassine ZRIBA <yassine.zriba@francetravail.fr>
2026-08-04 11:48:49 +0200

    Exercice 1 : créer un premier Observable

--- exercice1.mjs
+++ exercice1.mjs
@@ -1,5 +1,14 @@
 import { Observable } from 'rxjs';
 
 const prenoms$ = new Observable((subscriber) => {
- 
+    subscriber.next('Alice');
+    subscriber.next('Bob');
+    subscriber.next('Chloé');
+
+    subscriber.complete();
+});
+
+prenoms$.subscribe({
+  next: (prenom) => console.log(prenom),
+  complete: () => console.log('Terminé !'),
 });

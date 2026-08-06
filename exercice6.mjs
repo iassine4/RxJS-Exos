4e85aed58b0c9055a741b5b251b01f952762e2ea
Yassine ZRIBA <yassine.zriba@francetravail.fr>
2026-08-06 11:44:54 +0200

    Exercice 6 : tester les recherches parallèles avec mergeMap

---
+++ exercice6.mjs
@@ -0,0 +1,18 @@
+import { of } from 'rxjs';
+import { mergeMap, switchMap, delay } from 'rxjs/operators';
+
+function rechercherAPI(terme) {
+  const delaiAleatoire = Math.random() * 1000;
+
+  return of(`Résultats pour "${terme}"`).pipe(
+    delay(delaiAleatoire)
+  );
+}
+
+const recherches$ = of('ang', 'angular');
+
+recherches$.pipe(
+  mergeMap((terme) => rechercherAPI(terme))
+).subscribe((resultat) => {
+  console.log(resultat);
+});
